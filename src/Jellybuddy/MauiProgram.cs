c6c018ce97641f760d307b3da603793d23e3fe4f
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-09-04 00:42:30 +0100

    Started to shell the settings page

--- src/Jellybuddy/MauiProgram.cs
+++ src/Jellybuddy/MauiProgram.cs
@@ -6,6 +6,7 @@ using Jellybuddy.Services;
 using MauiIcons.Core;
 using MauiIcons.Material;
 using Microsoft.Extensions.Logging;
+using Syncfusion.Maui.Core.Hosting;
 using Syncfusion.Maui.Toolkit.Hosting;
 
 namespace Jellybuddy
@@ -23,6 +24,7 @@ namespace Jellybuddy
 			MauiAppBuilder builder = MauiApp.CreateBuilder();
 			builder
 				.ConfigureSyncfusionToolkit()
+				.ConfigureSyncfusionCore()
 				.UseMauiApp<App>()
 				.UseMauiCommunityToolkit()
 				.UseMauiIconsCore()
@@ -32,6 +34,21 @@ namespace Jellybuddy
 				{
 					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 					fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				})
+				.ConfigureMauiHandlers(handlers =>
+				{
+#if IOS
+					handlers.AddHandler(typeof(Entry), typeof(Microsoft.Maui.Handlers.EntryHandler));
+					Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoBorderEntry", (handler, view) =>
+					{
+						if (view is Entry)
+						{
+							handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear;
+							handler.PlatformView.Layer.BorderWidth = 0;
+							handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+						}
+					});
+#endif
 				});
 
 			builder.Services.AddSingleton<INavigationManager<Page>, NavigationManager>();
