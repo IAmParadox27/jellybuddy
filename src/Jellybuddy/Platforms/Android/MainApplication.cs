bfee0a062d16b1bed7d90590737db128e9133802
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-09-03 23:14:26 +0100

    Removed underline from default android Entry element. Changed search bar in users tab to be normal Entry rather than the SfTextInputLayout which looked a bit strange. Probably going to change all of them before releasing

--- src/Jellybuddy/Platforms/Android/MainApplication.cs
+++ src/Jellybuddy/Platforms/Android/MainApplication.cs
@@ -1,5 +1,7 @@
 ﻿using Android.App;
+using Android.Content.Res;
 using Android.Runtime;
+using Microsoft.Maui.Controls.PlatformConfiguration;
 
 namespace Jellybuddy
 {
@@ -9,6 +11,14 @@ namespace Jellybuddy
 		public MainApplication(IntPtr handle, JniHandleOwnership ownership)
 			: base(handle, ownership)
 		{
+			Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (handler, view) =>
+			{
+				if (view is Entry)
+				{
+					// Remove underline
+					handler.PlatformView.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+				}
+			});
 		}
 
 		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
